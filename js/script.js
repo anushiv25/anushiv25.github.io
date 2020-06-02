59466e0c68a869ef6ae21c4e4f3580713e21e5b8
anushiv25 <shuklanushiv@gmail.com>
2020-06-02 16:06:30 +0530

    Added count up in stats section and added contact section

--- js/script.js
+++ js/script.js
@@ -33,9 +33,11 @@ $(document).ready(function(){
     });
 
     var skillsTopOffset = $(".skillsSection").offset().top;
+    var statsTopOffset = $(".statsSection").offset().top;
+    var countupfinished = false;
 
     $(window).scroll(function(){
-        if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+        if(window.pageYOffset > skillsTopOffset - $(window).height() + 300) {
             $('.chart').easyPieChart({
                 easing: 'easeInOut',
                 barColor: '#fff',
@@ -48,7 +50,26 @@ $(document).ready(function(){
                 }
             });
         }
+
+        if(!countupfinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+            $('.count').each(function () {
+                console.log("here");
+                $(this).prop('Counter',0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 4000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.round(now));
+                    }
+                });
+            });
+            countupfinished = true;
+        }
+        
     });
+
+    $(".first").height($(".second").height()); //for making height of stats container same
 
 });
 
